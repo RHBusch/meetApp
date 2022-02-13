54296d0130f2e3be4b046b26a5528507536dbac2
RHBusch <91342001+RHBusch@users.noreply.github.com>
2022-02-13 11:48:19 -0500

    First Test Passed EventList

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
+import EventList from './EventList';
 
 export class App extends Component {
   render() {
     return (
       <div className="App">
+        <EventList />
 
       </div>
     );
