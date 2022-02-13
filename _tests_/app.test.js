b4d46e71acb905effbbe1c35ffd2e20065fdffe6
RHBusch <91342001+RHBusch@users.noreply.github.com>
2022-02-13 09:34:24 -0500

    Adding missing export in app.js + deleting comments for test

--- _tests_/app.test.js
+++ _tests_/app.test.js
@@ -14,12 +14,3 @@ describe('<App /> component', () => {
 
 });
 
-/* ^^^ This will fail initially because no EventList exists.
-I call the shallow() function using App as its parameter, 
-then set it to the constant AppWrapper. This allows me to 
-call the shallow-rendered App component within the test itself.
-The expect() function runs a search (find()) for EventList components 
-within AppWrapper, then compares the result 
-(i.e., how many EventList components exist within AppWrapper) with the expected result,
- which, in this case, is 1.
- */
