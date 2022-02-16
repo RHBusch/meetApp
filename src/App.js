6deb7492c18885226235a5f2ff41732dd8c8a7d5
RHBusch <91342001+RHBusch@users.noreply.github.com>
2022-02-16 07:32:52 -0500

    All tests pass and UI is up and running with npm run start

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import EventList from './EventList';
 import CitySearch from './CitySearch';
 import NumEvents from './NumEvents';
-import { getEvents } from './api';
+import { getEvents, extractLocations } from './api';
 
 
 export class App extends Component {
@@ -22,6 +22,17 @@ export class App extends Component {
         events: locationEvents
       })
     })
+  }
+  componentDidMount() { //Loading events when app loads. Using API call to save initial data to state. 
+    this.mounted = true; //Updating state only if this.mounted is true. 
+    getEvents().then((events) => {
+      if (this.mounted) {
+        this.setState({ events, locations: extractLocations(events) })
+      }
+    })
+  }
+  componentWillUnmount() {
+    this.mounted = false;
   }
 
   render() {
