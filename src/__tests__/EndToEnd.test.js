4daaff3d5834d994c533fc81722d17bdd563ad26
RHBusch <91342001+RHBusch@users.noreply.github.com>
2022-02-20 14:01:32 -0500

    refactored code browser.close was causing tests to fail early- moved it to afterAll()

--- src/__tests__/EndToEnd.test.js
+++ src/__tests__/EndToEnd.test.js
@@ -10,30 +10,30 @@ describe('show/hide an event detils', () => {
     let browser;
     let page;
     beforeAll(async () => {
-        jest.setTimeout(30000);
+        jest.setTimeout(300000);
         browser = await puppeteer.launch();
         page = await browser.newPage();
-        await page.goto('http://localhost:3000')
+        await page.goto('http://localhost:3000/meetapp')
         await page.waitForSelector('.event');
     })
-    afterAll(() => {
-        browser.close();
-    });
 
     test('an element is collapsed by default', async () => {
-        const eventDetails = await page.$('.event .event__Details');//Check variables here
+        const eventDetails = await page.$('.event .eventDescription');//Check variables here
         expect(eventDetails).toBeNull();
-        browser.close()
+
     })
     test('User can expand an event to see its details', async () => {
         await page.click('event .details-btn'); // confirm variables here
-        const eventDetails = await page.$('.event .event__Details');
+        const eventDetails = await page.$('.event .eventDescription');
         expect(eventDetails).toBeDefined();
-        browser.close();
+
     })
     test('User can collapse an event to hide its details', async () => {
         await page.click('.event .details-btn');
-        const eventDetails = await page.$('.event .event__Details');
+        const eventDetails = await page.$('.event .eventDescription');
         expect(eventDetails).toBeNull();
+    })
+    afterAll(() => {
+        browser.close();
     })
 })
