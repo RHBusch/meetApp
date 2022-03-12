d2fb540fc86c6bccbe86420f58aa212e3558cca7
RHBusch <91342001+RHBusch@users.noreply.github.com>
2022-03-12 07:00:34 -0500

    Testing with standard pie chart

--- src/EventGenre.js
+++ src/EventGenre.js
@@ -3,7 +3,7 @@ import {
     PieChart, Pie, Cell, ResponsiveContainer
 } from 'recharts';
 
-const EventGenre = ({ events }) => {
+/*const EventGenre = ({ events }) => {
     const [data, setData] = useState([]);
     useEffect(() => {
         const getData = () => {
@@ -19,15 +19,15 @@ const EventGenre = ({ events }) => {
     }, [events]
     )
 
-    const colors = ['#A3C4BC', '#BFD7B5', '#E7EFC5', '#F2DDA4', '#F4B393'];
+    const colors = ['#A3C4BC', '#BFD7B5', '#E7EFC5', '#F2DDA4', '#F4B393'];*/
 
-    /*const EventGenre = ({ events }) => {
-        const data = [
-            { name: 'Group A', value: 400 },
-            { name: 'Group B', value: 300 },
-            { name: 'Group C', value: 300 },
-            { name: 'Group D', value: 200 },
-        ];*/
+const EventGenre = ({ events }) => {
+    const data = [
+        { name: 'Group A', value: 400 },
+        { name: 'Group B', value: 300 },
+        { name: 'Group C', value: 300 },
+        { name: 'Group D', value: 200 },
+    ];
     return (
         <ResponsiveContainer height={400}>
             <PieChart width={400} height={400}>
@@ -41,7 +41,7 @@ const EventGenre = ({ events }) => {
                     dataKey="value"
                     label={({ name, percent }) => `${name} ${(percent * 100).toFixed(0)}%`}>
                     {data.map((entry, index) => (
-                        <Cell key={`cell-${index}`} fill={colors[index]} />))}
+                        <Cell key={`cell-${index}`} /*fill={colors[index]}*/ />))}
                 </Pie>
             </PieChart>
         </ResponsiveContainer>
