6f5dcd422ab8141c732701ca73be91ce7f0afb03
RHBusch <91342001+RHBusch@users.noreply.github.com>
2022-03-12 07:51:27 -0500

    Changing display on chart to %

--- src/EventGenre.js
+++ src/EventGenre.js
@@ -21,32 +21,6 @@ const EventGenre = ({ events }) => {
     const colors = ['red', 'orange', 'purple', 'green', 'blue'];
 
     console.log(getData());
-
-
-    /*const EventGenre = ({ events }) => {
-        const [data, setData] = useState([]);
-        useEffect(() => {
-            const getData = () => {
-                const genres = ['React', 'JavaScript', 'Node.js', 'jQuery', 'AngularJS'];
-                const data = genres.map((genre) => {
-                    const value = events.filter(({ summary }) =>
-                        summary.split('').includes(genre)).length;
-                    return { name: genre, value };
-                })
-                return data;
-            }
-            setData(() => getData());
-        }, [events])
-    
-        const colors = ['#A3C4BC', '#BFD7B5', '#E7EFC5', '#F2DDA4', '#F4B393'];*/
-
-    /*const EventGenre = ({ events }) => {
-        const data = [
-            { name: 'Group A', value: 400 },
-            { name: 'Group B', value: 300 },
-            { name: 'Group C', value: 300 },
-            { name: 'Group D', value: 200 },
-        ];*/
     return (
         <ResponsiveContainer height={400}>
             <PieChart width={400} height={400}>
@@ -57,7 +31,7 @@ const EventGenre = ({ events }) => {
                     labelLine={false}
                     outerRadius={80}
                     dataKey="value"
-                    label={({ name, value }) => `${name}: ${(value)}`}>
+                    label={({ name, percent }) => `${name}: ${(percent * 100).toFixed(0)}%`}>
                     {data.map((entry, index) => (
                         <Cell key={`cell-${index}`} fill={colors[index]} />
                     ))}
